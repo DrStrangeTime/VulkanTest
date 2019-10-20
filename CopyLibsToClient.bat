2e7f22e524eaa61aa1de0dbecc933c984a4d0ecc
DrStrangeTime <williamtwheate@hotmail.com>
2019-10-20 21:34:38 +0100

    New Update

--- CopyLibsToClient.bat
+++
@@ -1,8 +0,0 @@
-@ echo $PWD
-xcopy ExCore\bin\ExCore.dll Editor\bin\ /Y
-xcopy ExRenderer\bin\ExRenderer.dll Editor\bin\ /Y
-xcopy ExPhysics\bin\ExPhysics.dll Editor\bin\ /Y
-xcopy ExAudio\bin\ExAudio.dll Editor\bin\ /Y
-xcopy libs\glfw\lib\glfw3.dll Editor\bin\ /Y
-xcopy libs\glew\lib\glew32.dll Editor\bin\ /Y
-PAUSE
